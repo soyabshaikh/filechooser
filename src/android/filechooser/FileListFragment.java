5e2ca38f7868d1ffcef07f0ef04bf43705380283
soyabshaikh <soyab.s@applicationnexus.com>
2015-02-11 18:17:57 +0530

    Update FileListFragment.java

--- src/android/filechooser/FileListFragment.java
+++ src/android/filechooser/FileListFragment.java
@@ -26,6 +26,8 @@ import android.view.View;
 import android.widget.ListView;
 import java.io.File;
 import java.util.List;
+import org.apache.cordova.CordovaPlugin; 
+import org.apache.cordova.PluginResult;
 /**
  * Fragment that displays a list of Files in a given path.
  * 
