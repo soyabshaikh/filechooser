d483ad08ff73360700a81a76bd989decd8962c45
soyabshaikh <soyab.s@applicationnexus.com>
2015-02-11 18:42:40 +0530

    Update LocalStorageProvider.java

--- src/android/filechooser/LocalStorageProvider.java
+++ src/android/filechooser/LocalStorageProvider.java
@@ -58,7 +58,7 @@ public class LocalStorageProvider extends DocumentsProvider {
         row.add(Root.COLUMN_DOCUMENT_ID, homeDir.getAbsolutePath());
         row.add(Root.COLUMN_TITLE, "Internal storage");
         row.add(Root.COLUMN_FLAGS, Root.FLAG_LOCAL_ONLY | Root.FLAG_SUPPORTS_CREATE);
-        row.add(Root.COLUMN_ICON, this.cordova.getActivity().getResources().getIdentifier("ic_provider", "drawable", this.cordova.getActivity().getPackageName()));
+        row.add(Root.COLUMN_ICON, this.cordova.getActivity().getResources().getIdentifier("ic_provider", "drawable", "io.circuitmouse.app"));
         // These columns are optional
         row.add(Root.COLUMN_AVAILABLE_BYTES, homeDir.getFreeSpace());
         // Root.COLUMN_MIME_TYPE is another optional column and useful if you
